b5dcaa3417796e8ff4b5e41c057c339446ebc45d
komronz <pandacoder0820@gmail.com>
2022-07-28 19:16:33 +0500

    module3task5

--- src/main/java/lang/print/gaps/task5/TimeConvertor.java
+++ src/main/java/lang/print/gaps/task5/TimeConvertor.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task5;
 
 public class TimeConvertor {
     public void convert(float minutes) {
+        float seconds = 60 * minutes;
+        System.out.println(seconds);
     }
+
 }
